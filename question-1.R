df5770720f06d76583613e5316094daa766de040
Remco Niggebrugge <remco.niggebrugge@gmail.com>
2015-01-16 09:22:39 +0100

    question 1 code added

---
+++ question-1.R
@@ -0,0 +1,17 @@
+library(dplyr)
+
+# load data, files should be in current  working directory
+#scc <- readRDS("Source_Classification_Code.rds")
+#nei <- readRDS("summarySCC_PM25.rds")
+
+# group by year                                           #
+years              <- group_by(nei, year)
+
+# summarize over the created groups, creating a sum of total emissions per group (=per year)
+development  <- summarize(years, emissions=sum(Emissions))
+
+# plot the total emissions against year to see development. Force y-scale to start at 0
+# in order to get realistic impression of increase/decrease relative to total amount. 
+with( development, plot(year, emissions, type="l", col="blue", lwd=4, ylim=c(0,max(emissions))))
+
+print(development)
